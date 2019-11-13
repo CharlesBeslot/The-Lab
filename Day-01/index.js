4231d5cfb1250adfcd17ca6112e41e9f5b7fa1c4
Charles <charles@MBP-de-Charles.home>
2019-11-13 17:54:43 +0100

    task 7 completed

--- Day-01/index.js
+++ Day-01/index.js
@@ -4,10 +4,15 @@ require('./my_concat')
 require('./my_char_at')
 require('./my_includes')
 require('./is_empty')
+require('./my_insert')
+
+var sentence = 'The quick brown fox jumps over the lazy dog.'
+var word = 'fox '
 
 console.log("hello".my_to_upper_case())
 console.log("HELLO".my_to_lower_case())
 console.log("Hello".my_concat("I'm","so","bad"))
 console.log("I'm so bad".my_char_at(5))
-console.log("I'm so bad".my_includes("bad",1))
-console.log("".is_empty())
+console.log(`The word "${word}" ${sentence.my_includes(word)? 'is': 'is not'} in the sentence`)
+console.log(`The sentence ${sentence.is_empty()? 'is' : 'is not'} empty`)
+console.log(`${sentence.my_insert(word, 16)}`)
